4766a708cb44d357a893b4ad70b7ee8b87bf7d1c
FXWS56Y <extern.robert.piatek@cariad.technology>
2024-12-02 12:26:20 +0100

    Structure of day2 and read_report method implemented

--- day2/day2.py
+++ day2/day2.py
@@ -1,1 +1,37 @@
-# Ready to start
+"""The engineers are trying to figure out which reports are safe.
+The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing.
+So, a report only counts as safe if both of the following are true:
+
+- The levels are either all increasing or all decreasing.
+- Any two adjacent levels differ by at least one and at most three.
+"""
+
+def read_report(p) -> list[list[int]]:
+    """Read reports consisting of levels (elements) from file in form of list of lists of ints.
+
+    :param p: Path to file.
+    :return: Report of levels - a list of lists of ints.
+    """
+    report = []
+    with open(p, "r") as f:
+        for line in f:
+            report.append([int(n) for n in line.split()])
+    return report
+
+def check_if_levels_increasing_decreasing(levels: list[int]):
+    """Check if levels (elements) are all increasing or decreasing within the list.
+
+    :param levels: List of levels (elements) to check.
+    """
+
+def check_if_levels_differ_correctly(levels: list[int]):
+    """Check if levels (elements) differ by at least one and at most three within the list.
+
+    :param levels: List of levels (elements) to check.
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    report_of_levels = read_report(r"C:\Users\robert.piatek\OneDrive - Accenture\Desktop\AOC\AoC2024\day2\data.txt")
+    print(report_of_levels)
