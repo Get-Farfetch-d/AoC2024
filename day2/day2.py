5bce02f5428d949a60aad9ec3c939ed02bcac4b4
Jagoda Klos <jagoda.klos@accenture.com>
2024-12-02 16:28:24 +0100

    Update day2 with task 2 and delete stand-alone file.

--- day2/day2.py
+++ day2/day2.py
@@ -1,9 +1,17 @@
-"""The engineers are trying to figure out which reports are safe.
+""" TASK 1:
+The engineers are trying to figure out which reports are safe.
 The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing.
 So, a report only counts as safe if both of the following are true:
 
 - The levels are either all increasing or all decreasing.
 - Any two adjacent levels differ by at least one and at most three.
+
+TASK 2:
+The engineers are surprised by the low number of safe reports until they realize they forgot to tell you about the Problem Dampener.
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what
+would otherwise be a safe report. It's like the bad level never happened!
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe,
+the report instead counts as safe.
 """
 
 
@@ -52,9 +60,38 @@ def calculate_safe_reports(report_of_levels: list[list[int]]) -> int:
     """
     safe_reports = 0
     for _, single_report in enumerate(report_of_levels):
-        if check_if_levels_increasing_decreasing(single_report) and check_if_levels_differ_correctly(single_report):
+        if check_if_report_is_safe(single_report):
             safe_reports += 1
+        else:
+            if thanos_snap_one_level(single_report):
+                safe_reports += 1
     return safe_reports
+
+
+def thanos_snap_one_level(problematic_report: list[int]) -> bool:
+    """Remove singular level and check again the report.
+
+    :param problematic_report: Report to be tested again with singular level removed, causing issues otherwise.
+    :return: True if removing singular level made report safe, False otherwise.
+    """
+    for index, _ in enumerate(problematic_report):
+        fixed_report = problematic_report.copy()
+        del fixed_report[index]
+        if check_if_report_is_safe(fixed_report):
+            return True
+    return False
+
+
+def check_if_report_is_safe(single_report: list[int]) -> bool:
+    """Check if single report is considered safe or unsafe.
+
+    :param single_report: Report to be tested.
+    :return: True if report is safe, False otherwise.
+    """
+    if check_if_levels_increasing_decreasing(single_report) and check_if_levels_differ_correctly(single_report):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
