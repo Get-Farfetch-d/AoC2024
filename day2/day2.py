4d85ceffae186b53061d4e2cccb7dd3a47a6a2ff
FXWS56Y <extern.robert.piatek@cariad.technology>
2024-12-02 12:30:37 +0100

    Added skeleton for calculate_unsafe_reports

--- day2/day2.py
+++ day2/day2.py
@@ -6,6 +6,7 @@ So, a report only counts as safe if both of the following are true:
 - Any two adjacent levels differ by at least one and at most three.
 """
 
+
 def read_report(p) -> list[list[int]]:
     """Read reports consisting of levels (elements) from file in form of list of lists of ints.
 
@@ -18,17 +19,27 @@ def read_report(p) -> list[list[int]]:
             report.append([int(n) for n in line.split()])
     return report
 
+
 def check_if_levels_increasing_decreasing(levels: list[int]):
     """Check if levels (elements) are all increasing or decreasing within the list.
 
     :param levels: List of levels (elements) to check.
     """
 
+
 def check_if_levels_differ_correctly(levels: list[int]):
     """Check if levels (elements) differ by at least one and at most three within the list.
 
     :param levels: List of levels (elements) to check.
     :return:
+    """
+
+
+def calculate_unsafe_reports(report_of_levels: list[int]) -> int:
+    """Check how many unsafe single reports are within report of levels.
+
+    :param report_of_levels: List of reports of levels.
+    :return: Number of unsafe reports.
     """
 
 
