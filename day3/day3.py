f49f1119c97eaf7bf56049c3994a902482bc65c6
Jagoda Klos <jagoda.klos@accenture.com>
2024-12-03 07:54:14 +0100

    Print mapping

--- day3/day3.py
+++ day3/day3.py
@@ -5,19 +5,19 @@ def read_report(p):
     return data
 
 
-def add_correct_mul(mul_function):
-    """..."""
-    add_them = []
-    for input in mul_function.split("mul("):
-        if input[0].isdigit():
-            comma_index = input.find(",")
-            if input[0:comma_index].isdigit():
-                value_a = int(input[0:comma_index])
-                endbracket_index = input.find(")")
-                if input[comma_index + 1:endbracket_index].isdigit():
-                    value_b = int(input[comma_index + 1:endbracket_index])
-                    add_them.append(abs(value_a) * abs(value_b))
-    return sum(add_them)
+# def add_correct_mul(mul_function):
+#     """..."""
+#     add_them = []
+#     for input in mul_function.split("mul("):
+#         if input[0].isdigit():
+#             comma_index = input.find(",")
+#             if input[0:comma_index].isdigit():
+#                 value_a = int(input[0:comma_index])
+#                 endbracket_index = input.find(")")
+#                 if input[comma_index + 1:endbracket_index].isdigit():
+#                     value_b = int(input[comma_index + 1:endbracket_index])
+#                     add_them.append(abs(value_a) * abs(value_b))
+#     return sum(add_them)
 
 
 def dict_solution(mul_function):
@@ -34,16 +34,17 @@ def dict_solution(mul_function):
                     value_b = int(input[comma_index + 1:endbracket_index])
                     mapping.update({"mul(" + input: {"a": value_a, "b": value_b, "a * b": value_a * value_b}})
                     sumup += value_a * value_b
+    print(mapping)
     return sumup
 
 
 if __name__ == "__main__":
     report_of_levels = read_report(r"data.txt")
     print(f"data: {report_of_levels}")
-    print(f"add those: {add_correct_mul(report_of_levels)}")
+    # print(f"add those: {add_correct_mul(report_of_levels)}")
     print(f"dict solution: {dict_solution(report_of_levels)}")
 
     report_of_levels = read_report(r"test_data.txt")
     print(f"test_data: {report_of_levels}")
-    print(f"add those: {add_correct_mul(report_of_levels)}")
+    # print(f"add those: {add_correct_mul(report_of_levels)}")
     print(f"dict solution: {dict_solution(report_of_levels)}")
